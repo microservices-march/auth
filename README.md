30e6b15fabd1eb0b1465334e2e362d1c1045b978
RuncibleSpoon <robert.haynes@gmail.com>
2023-01-31 22:54:59 -0800

    initial

---
+++ README.md
@@ -0,0 +1,1 @@
+#This is the lab for the secrets management lesson# 
---
+++ apiclient/app.py
@@ -0,0 +1,14 @@
+import urllib.request
+import urllib.error
+import os
+
+jwt="Bearer " + os.environ['JWT']
+req = urllib.request.Request('http://localhost/')
+req.add_header("Authorization", jwt)
+try:
+      with urllib.request.urlopen(req) as response:
+         the_page = response.read()
+         message = response.getheader('X-MESSAGE')
+         print("200  " + message)
+except urllib.error.URLError as e:
+      print(str(e.code) + "  " + e.msg) 
