03fcbf78490a086d10a5dcd0ef42503156ec5e14
RuncibleSpoon <robert.haynes@gmail.com>
2023-02-01 10:09:44 -0800

    addded app versions

---
+++ apiclient/app_versions/medium_environment_variables.py
@@ -0,0 +1,16 @@
+import urllib.request
+import urllib.error
+import os
+jwt=""
+if "JWT" in os.environ:
+ jwt="Bearer " + os.environ.get("JWT")
+
+req = urllib.request.Request('http://host.docker.internal')
+req.add_header("Authorization", jwt)
+try:
+      with urllib.request.urlopen(req) as response:
+         the_page = response.read()
+         message = response.getheader('X-MESSAGE')
+         print("200  " + message)
+except urllib.error.URLError as e:
+      print(str(e.code) + "  " + e.msg) 
