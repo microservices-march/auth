9f0e43f8266fbbd0725292d981abb43edbead3e6
Alessandro Fael Garcia <alessfg@hotmail.com>
2023-02-14 22:19:37 +0100

    Cleanup code (#1)
    And add required community focused docs

--- apiclient/app_versions/medium_environment_variables.py
+++ apiclient/app_versions/medium_environment_variables.py
@@ -1,16 +1,17 @@
+import os
 import urllib.request
 import urllib.error
-import os
-jwt=""
-if "JWT" in os.environ:
- jwt="Bearer " + os.environ.get("JWT")
 
-req = urllib.request.Request('http://host.docker.internal')
+jwt = ""
+if "JWT" in os.environ:
+    jwt = "Bearer " + os.environ.get("JWT")
+
+req = urllib.request.Request("http://host.docker.internal")
 req.add_header("Authorization", jwt)
 try:
-      with urllib.request.urlopen(req) as response:
-         the_page = response.read()
-         message = response.getheader('X-MESSAGE')
-         print("200  " + message)
+    with urllib.request.urlopen(req) as response:
+        the_page = response.read()
+        message = response.getheader("X-MESSAGE")
+        print("200 " + message)
 except urllib.error.URLError as e:
-      print(str(e.code) + "  " + e.msg) 
+    print(str(e.code) + " " + e.msg)
